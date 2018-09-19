4481c9a7c4f385498d7d709ebefe029a75635495
sandip <sgadekar81@gmail.com>
2018-09-19 15:01:30 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// this is the entry point for your application
+export { StartModule } from './start.module';
---
+++ src/start.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [
+    ],
+    imports: [
+        
+    ],
+    entryComponents: [
+    ],
+    providers: [
+    ],
+    exports: [
+        
+    ]
+})
+
+export class JBStore {
+    constructor() { };
+}
